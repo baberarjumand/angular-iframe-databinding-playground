e1e8e44eb253adf05f7da53bce5602963d569ab1
Baber Arjumand <baber.arjumand@gmail.com>
2022-04-25 14:50:16 +0500

    added bootstrap via cdn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InnerComponent } from './inner/inner.component';
+import { MainOuterComponent } from './main-outer/main-outer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainOuterComponent,
+  },
+  {
+    path: 'inner',
+    component: InnerComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainOuterComponent } from './main-outer/main-outer.component';
+import { InnerComponent } from './inner/inner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainOuterComponent,
+    InnerComponent
   ],
   imports: [
     BrowserModule,
